d9c387f7c40c331b97912739e956c960d706cf8f
Alexia Kattah <alexiakattah@gmail.com>
2020-02-12 16:38:02 -0300

    send email

---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '084f8b6c20128d',
+    pass: '0a73a09905b162',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Amazon SES
+ * mailgun
+ * sparkpost
+ * mandril(mailchimpo)
+ *
+ * mailtrap(DEV)
+ */
